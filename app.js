acdf83dc5ab7e2b7975717b3f68687f57fe2d83d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-29 19:01:05 +0900

    ゴムゴムのボタンをクリックしたら正解git add .と表示される

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const answers = [
 ];
 const correct = 'ゴムゴム';
 
-//index.htmlから取ってくる
+//定数の文字列をHTMLに反映　　index.htmlから取ってくる
 document.getElementById('js-question').textContent = quiz;
 document.getElementsByTagName('button')[0].textContent = answers[0];
 document.getElementsByTagName('button')[1].textContent = answers[1];
@@ -17,3 +17,12 @@ document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
 document.getElementsByTagName('button')[4].textContent = answers[4];
 document.getElementsByTagName('button')[5].textContent = answers[5];
+
+//ボタンをクリックしたら正誤判定が出る
+document.getElementsByTagName('button')[0].addEventListener('click',() => {
+    if(correct === document.getElementsByTagName('button')[0].textContent){
+        window.alert('正解‼︎')
+    } else {
+        window.alert('不正解‼︎')
+    }
+});
