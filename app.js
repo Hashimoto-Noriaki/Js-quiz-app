2e28d043e7a87fae6f33b0595538277f8cff0b13
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-29 17:49:35 +0900

    不要なコメントを削除

--- app.js
+++ app.js
@@ -11,9 +11,3 @@ const correct = 'ゴムゴム';
 
 //index.htmlから取ってくる
 document.getElementById('js-question').textContent = quiz;
-// console.log(document.getElementById('js-question').textContent);
-// console.log(document.getElementById('js-question'));
-
-//divがたくさんあるので、ふさわしくない
-// document.getElementByTagName('div');
-// console.log(document.getElementByTagName('div'));
