17bcf02fd2dbf6c40227d77cb2bcabed2e9a963d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-30 00:06:04 +0900

    関数でまとめる

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ const answers = [
 ];
 const correct = 'ゴムゴム';
 
-//定数の文字列をHTMLに反映　　index.htmlから取ってくる
-document.getElementById('js-question').textContent = quiz;
-
-//同じような処理が続いたので、変数、定数にまとめる
-//$があることで、HTMLのオブジェクトが入っていると理解できる
+//同じような処理が続いたので、変数、定数にまとめる  $があることで、HTMLのオブジェクトが入っていると理解できる
 const $button = document.getElementsByTagName('button');
 
-//$buttonが何回も繰り返されていたので、リファクタリング
-let buttonIndex = 0;
-let buttonLength = $button.length;
-while(buttonIndex < buttonLength){
-    //命令
-    $button[buttonIndex].textContent = answers[buttonIndex];
-    buttonIndex++;
-}
+    //定数の文字列をHTMLに反映　　index.htmlから取ってくる
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = quiz;
+    //$buttonが何回も繰り返されていたので、リファクタリング
+    let buttonIndex = 0;
+    let buttonLength = $button.length;
+    while(buttonIndex < buttonLength){
+        //命令
+        $button[buttonIndex].textContent = answers[buttonIndex];
+        buttonIndex++;
+    }
+} 
+setupQuiz();
 
 //ボタンをクリックしたら正誤判定が出る
 document.getElementsByTagName('button')[0].addEventListener('click',() => {
