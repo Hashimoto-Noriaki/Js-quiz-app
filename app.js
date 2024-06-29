f287c7f5243651213bd0bb3c4957911892d862e1
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-30 01:38:42 +0900

    idをeのイベントオブジェクトなどを使ってif文をリファクタリング

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const correct = 'ゴムゴム';
 //同じような処理が続いたので、変数、定数にまとめる  $があることで、HTMLのオブジェクトが入っていると理解できる
 const $button = document.getElementsByTagName('button');
 
-    //定数の文字列をHTMLに反映　　index.htmlから取ってくる
+//定数の文字列をHTMLに反映　　index.htmlから取ってくる
+//関数で囲む
 const setupQuiz = () => {
     document.getElementById('js-question').textContent = quiz;
     //$buttonが何回も繰り返されていたので、リファクタリング
@@ -26,51 +27,35 @@ const setupQuiz = () => {
 } 
 setupQuiz();
 
-//ボタンをクリックしたら正誤判定が出る
-document.getElementsByTagName('button')[0].addEventListener('click',() => {
-    if(correct === $button[0].textContent){
-        window.alert('正解‼︎')
+const clickHandler = (e) => {
+    if(correct === e.target.textContent){
+        window.alert('正解‼︎');
     } else {
-        window.alert('不正解‼︎')
+        window.alert('不正解‼︎');
     }
+};
+
+//ボタンをクリックしたら正誤判定が出る　eはイベントオブジェクト　　イベントは色々な情報を持っている
+$button[0].addEventListener('click',(e) => {
+    clickHandler(e);
 });
 
-document.getElementsByTagName('button')[1].addEventListener('click',() => {
-    if(correct === $button[1].textContent){
-        window.alert('正解‼︎')
-    } else {
-        window.alert('不正解‼︎')
-    }
+$button[1].addEventListener('click',(e) => {
+    clickHandler(e);
 });
 
-document.getElementsByTagName('button')[2].addEventListener('click',() => {
-    if(correct === $button[2].textContent){
-        window.alert('正解‼︎')
-    } else {
-        window.alert('不正解‼︎')
-    }
+$button[2].addEventListener('click',(e) => {
+    clickHandler(e);
 });
 
-document.getElementsByTagName('button')[3].addEventListener('click',() => {
-    if(correct === $button[3].textContent){
-        window.alert('正解‼︎')
-    } else {
-        window.alert('不正解‼︎')
-    }
+$button[3].addEventListener('click',(e) => {
+    clickHandler(e);
 });
 
-document.getElementsByTagName('button')[4].addEventListener('click',() => {
-    if(correct === $button[4].textContent){
-        window.alert('正解‼︎')
-    } else {
-        window.alert('不正解‼︎')
-    }
+$button[4].addEventListener('click',(e) => {
+    clickHandler(e);
 });
 
-document.getElementsByTagName('button')[5].addEventListener('click',() => {
-    if(correct === $button[5].textContent){
-        window.alert('正解‼︎')
-    } else {
-        window.alert('不正解‼︎')
-    }
+$button[5].addEventListener('click',(e) => {
+    clickHandler(e);
 });
