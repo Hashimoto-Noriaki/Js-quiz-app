5b2b0abedfd8ca476e92507b8dc02c00518936f3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-29 17:42:28 +0900

    textContentでhtmlのテキスト情報を取得

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const answers = [
 const correct = 'ゴムゴム';
 
 //index.htmlから取ってくる
-console.log(document.getElementById('js-question'));
+console.log(document.getElementById('js-question').textContent)
+// console.log(document.getElementById('js-question'));
 
 //divがたくさんあるので、ふさわしくない
 // document.getElementByTagName('div');
