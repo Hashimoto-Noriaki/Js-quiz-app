0c4e768070cae1ce66098408e05c2029d033c2b1
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-29 17:55:57 +0900

    問題、質問の画面完成

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ const correct = 'ゴムゴム';
 
 //index.htmlから取ってくる
 document.getElementById('js-question').textContent = quiz;
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
+document.getElementsByTagName('button')[4].textContent = answers[4];
+document.getElementsByTagName('button')[5].textContent = answers[5];
