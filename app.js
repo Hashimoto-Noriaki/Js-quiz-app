5d16dee33438a8245240304edf8d36985bc8eccc
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-30 22:17:10 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const question = [
+const quiz = [
     {
-        quiz: 'ルフィの悪魔の実は?',
+        question: 'ルフィの悪魔の実は?',
         answers: [
             'ゴムゴム',
             'バラバラ',
@@ -36,7 +36,7 @@ const question = [
 ];
 
 const quizLength = quiz.length;
-let quizindex = 0;
+let quizIndex = 0;
 
 //同じような処理が続いたので、変数、定数にまとめる  $があることで、HTMLのオブジェクトが入っていると理解できる
 const $button = document.getElementsByTagName('button');
@@ -45,24 +45,35 @@ const buttonLength = $button.length;
 //定数の文字列をHTMLに反映　　index.htmlから取ってくる
 //関数で囲む
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = quiz;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     //$buttonが何回も繰り返されていたので、リファクタリング
     let buttonIndex = 0;
     let buttonLength = $button.length;
     while(buttonIndex < buttonLength){
         //命令
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
 } 
-setupQuiz();
+setupQuiz();//クイズの問題文や選択肢選択肢の書き換えをやっている命令
 
 const clickHandler = (e) => {
-    if(correct === e.target.textContent){
+    if(quiz[quizIndex].correct === e.target.textContent){
         window.alert('正解‼︎');
     } else {
         window.alert('不正解‼︎');
     }
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        //問題数があればここを実行
+        setupQuiz();
+    } else {
+        //問題数がなければここを実行
+        window.alert('終了');
+    }
+
 };
 
 //ボタンをクリックしたら正誤判定が出る　eはイベントオブジェクト　　イベントは色々な情報を持っている
